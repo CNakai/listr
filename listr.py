80632b735ab1d87b2296433cc51df69254edfd5d
Colter Nakai McAddis <cnm62@nau.edu>
2019-09-03 16:26:53 -0700

    Rewrite listr.py introducing 'request objects' & removing formatting
    The whole listr.py file was rewritten to simplify what it does.  Now, it is no
    longer concerned with sorting or formatting the results.  It merely gathers data
    and then hands it off to a presentation layer which handles all formatting and
    organization.
    
    To facilitate this tightening of scope, the main data gathering pipeline now
    works like this:
      1. Retrieve card requests from the user (now supports quantities and printing
      preferences)
      2. Get the card data associated with the requested cards from all_cards.json
      3. Merge select card data with the request object to create an enriched
      request
      4. Pass enriched requests off to the appropriate presentation layer

--- listr.py
+++ listr.py
@@ -1,161 +1,155 @@
 import json
+import re
 import sys
 from mako.template import Template
 from mako.lookup import TemplateLookup
 
-CUTOFF_SET_CODE = 'HOU'
-SET_RELEASE_DATE_FILE_PATH = 'data/set_release_dates.json'
-ALL_CARDS_FILE_PATH = 'data/all_cards.json'
+def _get_all_card_data(all_card_data_path):
+    with open(all_card_data_path, encoding='utf-8') as card_data_file:
+        return json.load(card_data_file)
+
+
+def _get_sets_requiring_SRCN_order(config_file_path):
+    with open(config_file_path, encoding='utf-8') as config_file:
+        config_lines = config_file.readlines()
+    return list(map(lambda line: line.strip(), config_lines))
+
+
+ALL_CARD_DATA_PATH = 'data/all_cards.json'
+ALL_CARD_DATA = _get_all_card_data(ALL_CARD_DATA_PATH)
+
 CONFIG_FILE_PATH = 'data/config.cnf'
-PAGE_SEPARATOR = '------------------------------------------------------------'
+SETS_REQUIRING_SRCN_ORDER = _get_sets_requiring_SRCN_order(CONFIG_FILE_PATH)
+
+# SRCN Order = set, rarity, color, name order
+#   CN Order = color, name order
 
 
-# SRCNO = set, rarity, color, name order
-#   CNO = color, name order
-
-
-# Point to the location of the card list and where you want the output
-# card_objects is built in this order:
-#    open the user's cardlist and make it into a python list
-#    open the all_cards.json and return all json entries that match the cards
-#      in the card list; we can ditch the card list after this step
-#    card_objects is a list of dictionaries
-
-# Figure out which sets are standard so they can be formatted in SRCNO or CNO
 def main():
-    (card_list_file_path, formatted_list_output_path) = get_cmd_args()
+    (requests_file_path, output_path) = get_cmd_args()
 
-    print(f"Reading Card List from {card_list_file_path} ...")
-    requested_cards = get_card_objects(get_card_name_list(card_list_file_path))
-    print(f"Done building card_objects")
-
-    # Commenting this out for now
-    #sets_requiring_SRCNO = get_all_sets_after(CUTOFF_SET_CODE)
-
-    sets_requiring_SRCNO = get_standard_sets(CONFIG_FILE_PATH)
-    (listings_for_SRCNO, listings_for_CNO) = generate_listings(requested_cards,
-                                sets_requiring_SRCNO)
-
-    SRCNO_listings = SRCNO_sort(listings_for_SRCNO)
-    CNO_listings = CNO_sort(listings_for_CNO)
-
-    output_listings(SRCNO_listings + CNO_listings,
-                    formatted_list_output_path)
-
-
-def get_standard_sets(config_file):
-    ''' Get the standard sets from the config file '''
-    standard_sets = []
-    with open(config_file, encoding='utf-8') as read_file:
-        config_lines = read_file.readlines()
-        for line in config_lines:
-            standard_sets.append(line.strip())
-    return standard_sets
-
-
-# Needs work; not sure it's doing what we originally envisioned
-def generate_listings(card_list, sets_requiring_SRCNO):
-    ''' Builds a list of cards grouped based on SRCNO or CNO
-    listing. '''
-    # Currently, this just splits the list of dictionaries into two lists
-    #so we'll have to make some changes to this
-    SRCNO_cards = []
-    CNO_cards = []
-    seen_CNO_card_names = set()
-    for card_name in card_list:
-        current_card_printings = card_name.get('printings')
-        for printing in current_card_printings:
-            if card_name['rarity'] in ('rare', 'mythic') or \
-               printing in sets_requiring_SRCNO:
-                SRCNO_cards.append(create_listing(card_name, printing, 'SRCNO'))
-            else:
-                CNO_listing = create_listing(card_name, printing, 'CNO')
-                if CNO_listing['name'] not in seen_CNO_card_names:
-                    CNO_cards.append(CNO_listing)
-                    seen_CNO_card_names.add(CNO_listing['name'])
-    return (SRCNO_cards, CNO_cards)
-
-
-def create_listing(card_object, printing, sort_type):
-    ''' Puts a card object into SRCNO format '''
-    listing = {
-        'set': printing,
-        'rarity':card_object['rarity'],
-        'color': card_object['colors'],
-        'name': card_object['name'],
-        'sort_type': sort_type
-    }
-    return listing
-
-
-def SRCNO_sort(listings):
-    return sorted(sorted(CNO_sort(listings), key=lambda l: l['rarity']),
-                  key=lambda l: l['set'])
-
-
-def CNO_sort(listings):
-    return sorted(sorted(listings, key=lambda l: l['name']),
-                  key=lambda l: l['color'])
-
-
-# This may be bugged; gonna test it, standalone, in IDLE
-def get_card_name_list(card_list_file_path):
-    ''' Builds a list of a cards using a user provided file \n
-    The file will be provided when the listr.py script is run through the
-    CLI '''
-    card_list = []
-    with open(card_list_file_path) as read_file:
-        for card in read_file:
-            card_list.append(card.strip())
-        return card_list
-
-
-def get_all_cards_json(all_cards_file_path):
-    ''' Helper function for get_card_objects '''
-    with open(all_cards_file_path, encoding='utf-8') as read_file:
-        return json.load(read_file)
-
-
-def get_card_objects(card_list):
-    ''' returns a list of dictionaries using all_cards.json \n
-    card_list : a list of cards built using get_card_name_list() '''
-    found_cards = []
-    all_cards_json = get_all_cards_json(ALL_CARDS_FILE_PATH)
-    for card_name in card_list:
-        card_object = all_cards_json.get(card_name.upper(), "NOT FOUND")
-        if card_object == "NOT FOUND":
-            print(f"{card_name} not found!")
-        else:
-            found_cards.append(card_object)
-    print(f"Found {len(found_cards)} / {len(card_list)} cards")
-    return found_cards
+    # TODO: Reinstate summary output
+    (requests, unparseable_lines) = get_requests(requests_file_path)
+    print(unparseable_lines) # TODO: prettier output
+    (enriched_requests, requests_for_unknown_cards) = enrich_requests(requests)
+    print(requests_for_unknown_cards)
+    output_enriched_requests(enriched_requests, output_path)
 
 
 def get_cmd_args():
-    ''' Helper function to get CLI arguments '''
     return (sys.argv[1], sys.argv[2])
 
 
-def output_listings(listings, output_file_path):
-    if output_file_path.split('.')[-1] == 'html':
-        output_func = output_html_listings
+def get_requests(card_requests_file_path):
+    '''Creates dictionaries representing card requests from the file at the
+    provided path.  The request file should contain one request per line
+    consisting of a quantity, the card's name, and any restrictions on the
+    printings desired (preferred, excluded, and/or required).  Any unparseable
+    lines in the file (along with their line numbers) are stored in a list that
+    is returned along with the card request dictionaries.
+
+    '''
+    with open(card_requests_file_path) as requests_file:
+        request_lines = requests_file.readlines()
+
+    requests = []
+    unparseable_lines = [] #FIXME: Better name, it's a list of triples
+    for (line_number, request_line) in enumerate(request_lines, start=1):
+        try:
+            requests.append(parse_request_line(request_line, line_number))
+        except SyntaxError as error:
+            unparseable_lines.append((line_number, request_line, str(error)))
+    return (requests, unparseable_lines)
+
+
+def parse_request_line(request_line, line_number):
+    request_line = request_line.strip()
+    request_line_regexp = r"(^[0-9]{0,2}\s+)?([\w/', \-]+(?!\|))(?:(?:\s+\|)((?: \w{3,4})+)$)?"
+
+    result = re.match(request_line_regexp, request_line, flags=re.MULTILINE)
+    if not result:
+        raise SyntaxError(f"Malformed request on line {line_number}")
+    (quantity, name, printing_prefs) = result.groups()
+
+    try:
+        quantity = int(quantity.strip()) if quantity else 1
+    except ValueError:
+        raise SyntaxError(f"Invalid quantity on line {line_number}")
+
+    if request_line[result.end(2):] != '' and printing_prefs is None:
+        raise SyntaxError(f"Invalid printing preferences on line {line_number}")
+
+    if printing_prefs:
+        printing_prefs = printing_prefs.strip().split(' ')
+
+    return {
+        'name': name.strip(),
+        'quantity': quantity,
+        'printing_prefs': printing_prefs,
+        'line_number': line_number
+    }
+
+
+def enrich_requests(requests):
+    enriched_requests = []
+    requests_for_unknown_cards = []
+    for request in requests:
+        card_data = lookup_card_data_by_name(request['name'], ALL_CARD_DATA)
+        if card_data:
+            enriched_requests.append(enrich_request(request, card_data))
+        else:
+            requests_for_unknown_cards.append(request)
+    return (enriched_requests, requests_for_unknown_cards)
+
+
+def lookup_card_data_by_name(card_name, card_data):
+    if '//' in card_name:
+        card_name = card_name.split('//')[0].strip()
+    return card_data.get(card_name.upper())
+
+
+def enrich_request(request, card_data):
+    enriched_request = {**request, **card_data}
+    enriched_request['printings'] = get_orderings_for_printings(card_data)
+    del enriched_request['line_number']
+    return enriched_request
+
+
+def get_orderings_for_printings(card_data):
+    printing_ordering_pairs = {}
+    for printing in card_data['printings']:
+        if card_data['rarity'] in ('rare', 'mythic') \
+           or printing in SETS_REQUIRING_SRCN_ORDER:
+            ordering = 'SRCN_order'
+        else:
+            ordering = 'CN_order'
+
+        printing_ordering_pairs[printing] = 'SRCN_order'
+    return printing_ordering_pairs
+
+
+
+def output_enriched_requests(enriched_requests, output_path):
+    if output_path.split('.')[-1] == 'html':
+        output_func = output_html
     else:
-        output_func = output_python_listings
-    return output_func(listings, output_file_path)
+        output_func = output_python
+    return output_func(enriched_requests, output_path)
 
 
-def output_python_listings(listings, output_file_path):
-    with open(output_file_path, 'w') as f:
-        f.write('\n'.join([str(listing) for listing in listings]))
-
-
-def output_html_listings(listings, output_file_path):
+def output_html(enriched_requests, output_path):
     template_lookup = TemplateLookup(directories=['./'])
     output_template = Template(filename="templates/out_list_template.mako",
                                lookup=template_lookup)
-    listings_output = output_template.render(listings=listings)
-    with open(output_file_path, 'w', encoding='utf-8') as f:
+    output = output_template.render(requests=enriched_requests)
+    with open(output_path, 'w', encoding='utf-8') as f:
         f.write(listings_output)
+
+
+def output_python(enriched_requests, output_path):
+    with open(output_path, 'w') as f:
+        f.write('\n'.join([str(request) for request in enriched_requests]))
+
 
 if __name__ == '__main__':
     main()
